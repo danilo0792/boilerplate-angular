92827fb96e57a4f19c56ad3130ed6a93bc9dc005
danilo0792 <danilo_sp06@hotmail.com>
2014-10-28 21:52:21 -0200

    inclusão de pasta public com arquivos mock e inserção da task clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,10 @@
 module.exports = function(grunt){
 
   grunt.initConfig({
-    // ------------
-    // task copy //
-    // ------------
+    // -----------------------------------
+    // task copy                        //
+    // Realiza um backup do seu projeto //
+    // -----------------------------------
     copy:{
       // -------------
       // target public
@@ -30,9 +31,32 @@ module.exports = function(grunt){
         // -----------------------------------------------
         dest  : 'dist'
       }
+    },
+
+    clean: {
+
+      // Se apargarmos um determinado arquivo, quando formos executar a task copy, esta pasta ou arquivo não será excluiída
+      // portanto, devemos apagar a pasta dist e criá-la novamente, para apagar a pasta dist
+      // utilizamos a task clean
+      dist:{
+        // -----------------------------------------
+        // Definindo o diretório que será apagado //
+        // -----------------------------------------
+        src: 'dist'
+      }
+
     }
   });
+  
+  // Registrando e definindo a ordem de execução das tasks
+  // grunt dist executa a task clean e depois a copy
+  grunt.registerTask('dist', ['clean','copy']);
 
-  // Registrando a tarefa grunt-contrib-copy 
+  grunt.registerTask('default',['dist'])
+  
+  // ------------------------------------------
+  // Registrando a tarefa grunt-contrib-copy //
+  // ------------------------------------------
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 }
